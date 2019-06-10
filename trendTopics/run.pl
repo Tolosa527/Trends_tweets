fe6c343a80bfe0b3aad389dc0c1ba47a5867d4fd
Matias Nicolas Zulberti <matias.zulberti@LYV.livenation.com>
2019-06-09 21:07:34 -0300

    litle change

--- trendTopics/run.pl
+++ trendTopics/run.pl
@@ -1,19 +1,34 @@
 #!/usr/bin/perl
+use utf8;
 use strict;
 use warnings;
 use Data::Dumper;
 use JSON;
 
+# Import script return via STDOUT
 my $posts = `node main.js`;
 
-#$posts =~ s/^\'+/"/;
 my $perl_scalar = JSON->new->utf8->decode($posts);
 
-#if($perl_scalar->{status} eq "true"){
-	print "Nombre: ".$perl_scalar->{name}."\n";
-	print "Tweet: ".$perl_scalar->{text}."\n";
-# }else{
-# 	print "Error occurs.\n"
-# }
+my $name = _trimSpaces($perl_scalar->{name});
+my $twet = _trimSpaces($perl_scalar->{text});
+
+#$posts =~ s/^\'+/"/;
+# print STDOUT Data::Dumper::Dumper($perl_scalar);
+# print STDOUT Data::Dumper::Dumper(${$perl_scalar->{status}});
+
+if($perl_scalar->{status} eq "1"){
+	print "Nombre: ".$name."\n";
+	print "Tweet: ".$twet."\n";
+}else{
+	print "Algo salió mal";
+}
+
+sub _trimSpaces {
+	my $arg = shift;
+	$arg =~ s/\s+/ /g;
+	return $arg;
+	#print STDOUT Data::Dumper::Dumper($string);
+}
 
 1;
